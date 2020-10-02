567bc04ee5cbe105aad56e84056785f8c2344eb5
Anderley Candela Peña <anderleyc@gmail.com>
2020-10-02 12:21:38 -0500

    pruebas

--- index.js
+++ index.js
@@ -14,5 +14,11 @@ app.set('port', process.env.PORT || 5000)
 
 app.get('/', (req, res) => res.send(req.body))
 
+app.post('/webhook', async function(req, res) {
+    var data = req.body.hola
+    console.log(req.body)
+    res.send("_hola: " + data)
+})
+
 
 app.listen(app.get('port'), () => console.log(`Mati Webhook app listening on port ${app.get('port')}!`))
