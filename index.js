6a16f62430d5fe774804d813bbc1b7b0dd56d876
Anderley Candela Peña <anderleyc@gmail.com>
2020-10-02 14:47:04 -0500

    estabilizacion

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const rp = require('request-promise')
-
+const bodyParser = require('body-parser')
 const express = require('express')
 const { response } = require('express')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(bodyParser.json())
 app.set('trust proxy', true);
 
 
@@ -14,10 +15,17 @@ app.set('port', process.env.PORT || 5000)
 
 app.get('/', (req, res) => res.send(req.body))
 
-app.post('/webhook', async function(req, res) {
-    var data = req.body.hola
-    console.log(req.body)
-    res.send("_hola: " + data)
+
+app.post('/', (req, res) => {
+    console.log("Response:" + req.body.data)
+    res.send(req.body)
+})
+
+
+app.post('/webhook', (req, res) => {
+    var data = req.body
+    console.log(JSON.stringify(data))
+    res.send(JSON.stringify(data))
 })
 
 
