d89f99fd9eecc47981237b66c5ae429e819e5d2e
Anderley Candela Peña <anderleyc@gmail.com>
2020-10-05 12:40:04 -0500

    Modificacion link api

--- index.js
+++ index.js
@@ -15,16 +15,34 @@ app.set('port', process.env.PORT || 5000)
 
 app.get('/', (req, res) => res.send(req.body))
 
-
-app.post('/', (req, res) => {
-    console.log("Response:" + req.body.data)
-    res.send(req.body)
-})
-
-
 app.post('/webhook', (req, res) => {
+    var responseJSON = ''
     var data = req.body
-    console.log(JSON.stringify(data))
+    var eventName = req.body.eventName
+    if (eventName == 'verification_completed') {
+        var options = {
+            method: 'POST',
+            uri: 'http://200.121.128.122:8010/LOLIMSASERx/rest/webhook',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: {
+                Request: JSON.stringify(data)
+            },
+            json: true,
+        }
+
+        rp(options)
+            .then(function(response) {
+                responseJSON = JSON.stringify(response)
+                console.log(responseJSON)
+            })
+            .catch(function(err) {
+                success = false
+                responseJSON = JSON.stringify(err)
+            })
+    }
+    console.log("HEY: " + responseJSON)
     res.send(JSON.stringify(data))
 })
 
